6270da605affafde28632942bd3761b6d96d1e79
jeffferson99 <49041684+jeffferson99@users.noreply.github.com>
2020-01-29 10:28:28 -0500

    deleted lock file

--- src/containers/Dashboard.js
+++ src/containers/Dashboard.js
@@ -1,5 +1,4 @@
 import React from "react";
-import theme from "../theme/AdminTheme";
 import {
   Heading1,
   Heading2,
@@ -21,28 +20,74 @@ const Dashboard = () => {
       <Heading1 as="h1" text="Dashboard" />
 
       <Display1 as="h1" text="display1" />
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laborum, cum?
+        Incidunt ipsam necessitatibus quisquam repellendus quibusdam ad
+        voluptatibus saepe cumque.
+      </p>
+
       <Display2 as="h1" text="display2" />
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laborum, cum?
+        Incidunt ipsam necessitatibus quisquam repellendus quibusdam ad
+        voluptatibus saepe cumque.
+      </p>
+
       <Display3 as="h1" text="display3" />
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laborum, cum?
+        Incidunt ipsam necessitatibus quisquam repellendus quibusdam ad
+        voluptatibus saepe cumque.
+      </p>
+
       <Display4 as="h1" text="display4" />
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laborum, cum?
+        Incidunt ipsam necessitatibus quisquam repellendus quibusdam ad
+        voluptatibus saepe cumque.
+      </p>
 
       <Heading1 as="h1" text="heading1" />
-      <Heading2 as="h2" text="heading2" />
-      <Heading3 as="h3" text="heading3" />
-      <Heading4 as="h4" text="heading4" />
-      <Heading5 as="h5" text="heading5" />
-      <Heading6 as="h6" text="heading6" />
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laborum, cum?
+        Incidunt ipsam necessitatibus quisquam repellendus quibusdam ad
+        voluptatibus saepe cumque.
+      </p>
 
-      <section>
-        <p>sssssssss</p>
-        <p>ddddddd</p>
-        <p>xxxxxxxxxxxxxdd</p>
-        <p>dcdcdcdcdd</p>
-        <p>
-          <span style={{ fontFamily: theme.fonts.body }}>
-            {theme.fonts.body} this is just for testing
-          </span>
-        </p>
-      </section>
+      <Heading2 as="h2" text="heading2" />
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laborum, cum?
+        Incidunt ipsam necessitatibus quisquam repellendus quibusdam ad
+        voluptatibus saepe cumque.
+      </p>
+
+      <Heading3 as="h3" text="heading3" />
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laborum, cum?
+        Incidunt ipsam necessitatibus quisquam repellendus quibusdam ad
+        voluptatibus saepe cumque.
+      </p>
+
+      <Heading4 as="h4" text="heading4" />
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laborum, cum?
+        Incidunt ipsam necessitatibus quisquam repellendus quibusdam ad
+        voluptatibus saepe cumque.
+      </p>
+
+      <Heading5 as="h5" text="heading5" />
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laborum, cum?
+        Incidunt ipsam necessitatibus quisquam repellendus quibusdam ad
+        voluptatibus saepe cumque.
+      </p>
+
+      <Heading6 as="h6" text="heading6" />
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laborum, cum?
+        Incidunt ipsam necessitatibus quisquam repellendus quibusdam ad
+        voluptatibus saepe cumque.
+      </p>
     </section>
   );
 };
